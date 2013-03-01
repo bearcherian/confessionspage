e78044481bbb90c8094273e2a347b90a7cb4a53a
bearcherian <cherian.benjamin@gmail.com>
2013-03-01 19:26:00 +0000

    adding initial confesiso form

---
+++ confess.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+
+<form action="process" method="post">
+<textarea id="confession" name="confession" rows="5" cols="50"></textarea>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
