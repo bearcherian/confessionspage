827ce6987435e755304f13cbe67a857ab7e41847
bearcherian <cherian.benjamin@gmail.com>
2013-03-12 23:20:03 +0000

    Using new DB class

--- config/app.php
+++ config/app.php
@@ -1,4 +1,6 @@
 <?php
+require_once('db.php');
+
 $app_accesstoken = "340879852685280|RhkelskXPauQjL1A0Rb3-dM02ok";
 $addurl = "http://www.facebook.com/add.php?api_key=340879852685280&pages";
 
@@ -8,10 +10,9 @@ $domain = ($domain == "pre") ? "temple" : "pre";
 
 function getPageToken($domainString) {
 	try {
-	        $tokenStatement = $dbconn->prepare('SELECT access_token FROM pages WHERE cp_domain = :domain;');
-	        $tokenStatement->execute( array(':domain' => $domainString));
-		$result = $tokenStatement->fetch();
-	        $aToken = $result["acces_token"];
+		$db = new Database();
+		$result = $db->query('SELECT access_token FROM pages WHERE cp_domain = :domain;',array(':domain' => $domainString));
+	        return $result[0]["access_token"];
 
 	} catch(PDOException $e) {
         	echo "Could not find proper configuration for " . $domainString . ".confessionspage.com.";
