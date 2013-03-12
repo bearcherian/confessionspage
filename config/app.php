c670e3c5202e909ad61672511cf20ba3a5ed6038
bearcherian <cherian.benjamin@gmail.com>
2013-03-12 20:47:29 +0000

    added function to obtain page token.

--- config/app.php
+++ config/app.php
@@ -5,4 +5,16 @@ $addurl = "http://www.facebook.com/add.php?api_key=340879852685280&pages";
 $domain = array_shift(explode(".",$_SERVER['HTTP_HOST']));
 
 $domain = ($domain == "pre") ? "temple" : "pre";
+
+function getPageToken($domainString) {
+	try {
+	        $tokenStatement = $dbconn->prepare('SELECT access_token FROM pages WHERE cp_domain = :domain;');
+	        $tokenStatement->execute( array(':domain' => $domainString));
+		$result = $tokenStatement->fetch();
+	        $aToken = $result["acces_token"];
+
+	} catch(PDOException $e) {
+        	echo "Could not find proper configuration for " . $domainString . ".confessionspage.com.";
+	}
+}
 ?>
