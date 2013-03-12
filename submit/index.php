39181007bd19c8f23079540e3d1013e235ae8e56
bearcherian <cherian.benjamin@gmail.com>
2013-03-12 23:20:30 +0000

    Submitted data is now entered into the database

--- submit/index.php
+++ submit/index.php
@@ -3,9 +3,23 @@ include_once('../config/app.php');
 include_once('../config/db.php');
 include_once('../filter/filters.php');
 
-$confession = $_POST['confession'];
+$confession = isset($_POST['confession']) ? $_POST['confession'] : null;
 $ip = $_SERVER['REMOTE_ADDR'];
-$ipProxy = $_SERVER['HTTP_X_FORWARDED_FOR'];
+$ipProxy = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : null;
 
-echo "Confession: " . $confession . "<br />Ip: " . $ip . "<br />Proxy: " . $ipProxy;
+//echo "Confession: " . $confession . "<br />Ip: " . $ip . "<br />Proxy: " . $ipProxy;
+
+$statement = "INSERT INTO " . $domain . "_posts(ip_address, ip_proxy, post, post_status) " .
+		"VALUES (:ip, :proxy, :post,'new');";
+$values = array(':ip' => $ip,
+		':proxy' => $ipProxy,
+		':post' => $confession);
+
+//print_r($values);
+
+$db = new Database();
+$db->connect();
+$db->insert($statement,$values);
+$db->close();
+
 ?>
