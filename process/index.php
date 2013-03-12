7b4ef04bca41badd9fdfdb62705a91b913959f82
bearcherian <cherian.benjamin@gmail.com>
2013-03-12 20:25:17 +0000

    Properly connects to DB and retrieves token.

--- process/index.php
+++ process/index.php
@@ -4,12 +4,13 @@ include_once('../config/db.php');
 include_once('../filter/filters.php');
 
 try {
-	$tokenStatement = $dbconn->prepare('SELECT access_token FROM pages WHERE cp_domain = :domain');
-	$token 	= $tokenStatement->execute( array(':domain',$domain));
-	echo $aToken;
+	$tokenStatement = $dbconn->prepare('SELECT access_token FROM pages WHERE cp_domain = :domain;');
+	$tokenStatement->execute( array(':domain' => $domain));
+	$result = $tokenStatement->fetch();
+	$aToken = $result["acces_token"];
 
 } catch(PDOException $e) {
-	
+	echo "Could not find proper configuration for " . $domain . ".confessionspage.com.";
 }
 
 $confession = $_POST['confession'];
