aa273c6719e7710939937ef175f14ac25b529b16
bearcherian <cherian.benjamin@gmail.com>
2013-03-13 04:43:17 +0000

    post process now properly posts to facebook

--- process/post.php
+++ process/post.php
@@ -1,13 +1,32 @@
 <?php
-include_once '../config/app.php';
-include_once '../config/domain.php';
+require_once '../config/app.php';
+require_once '../config/domain.php';
+require_once '../config/confession.php';
 
-$postURL = "https://graph.facebook.com/".$pageid."/feed";
+$postId = (isset($_POST['message'])) ? $_POST['message'] : null;
+$domain = new Domain();
 
-$data = array( 
-	'message' => $_POST['message'],
-	'access_token' => $app_accesstoken
-);
-	
+if ($postId == null) {
 
+	$confession = new Confession($postId);
+	$message = "Testing";
+	$pageToken = $domain->getPageToken();
+	$pageId = $domain->getPageId();
+
+	$postURL = "https://graph.facebook.com/".$pageId."/feed";
+
+	$data = array( 
+		'message' => $message,
+		'access_token' => $pageToken
+	);
+
+	// use key 'http' even if you send the request to https://...
+	$options = array('http' => array('method'  => 'POST','content' => http_build_query($data)));
+	$context  = stream_context_create($options);
+	$result = file_get_contents($postURL, false, $context);
+
+	$fb = json_decode($result,true);	
+} else {
+	echo "No post specified.";
+}
 ?>
