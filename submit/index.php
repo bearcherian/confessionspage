7a0654b824e72cba78c8def3b3645d15dd2f99f2
bearcherian <cherian.benjamin@gmail.com>
2013-03-13 04:41:23 +0000

    Using domain class and single post domain. Might change this back to separate

--- submit/index.php
+++ submit/index.php
@@ -1,7 +1,9 @@
 <?php
-include_once('../config/app.php');
-include_once('../config/db.php');
-include_once('../filter/filters.php');
+require_once('../config/app.php');
+require_once('../config/db.php');
+require_once('../config/domain.php');
+
+$domain = new Domain();
 
 $confession = isset($_POST['confession']) ? $_POST['confession'] : null;
 $ip = $_SERVER['REMOTE_ADDR'];
@@ -9,11 +11,13 @@ $ipProxy = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_
 
 //echo "Confession: " . $confession . "<br />Ip: " . $ip . "<br />Proxy: " . $ipProxy;
 
-$statement = "INSERT INTO " . $domain . "_posts(ip_address, ip_proxy, post, post_status) " .
-		"VALUES (:ip, :proxy, :post,'new');";
+$statement = "INSERT INTO posts(ip_address, ip_proxy, post, post_status,domain) " .
+		"VALUES (:ip, :proxy, :post,'new',:domain);";
+
 $values = array(':ip' => $ip,
 		':proxy' => $ipProxy,
-		':post' => $confession);
+		':post' => $confession,
+		':domain' => $domain->getDomain());
 
 //print_r($values);
 
