205cca7bd0ba5504a9c335596f8c3d57325a4b88
bearcherian <cherian.benjamin@gmail.com>
2013-03-13 04:40:20 +0000

    Removing extra functions that are now in other classes. app only has configurations now

--- config/app.php
+++ config/app.php
@@ -1,21 +1,7 @@
 <?php
-require_once('db.php');
+   $app_id = "340879852685280";
+   $app_secret = "711927c306a77b9bbe1277927e08e102";
+   $app_accesstoken = "340879852685280|RhkelskXPauQjL1A0Rb3-dM02ok";
+   $addurl = "http://www.facebook.com/add.php?api_key=340879852685280&pages";
 
-$app_accesstoken = "340879852685280|RhkelskXPauQjL1A0Rb3-dM02ok";
-$addurl = "http://www.facebook.com/add.php?api_key=340879852685280&pages";
-
-$domain = array_shift(explode(".",$_SERVER['HTTP_HOST']));
-
-$domain = ($domain == "pre") ? "temple" : "pre";
-
-function getPageToken($domainString) {
-	try {
-		$db = new Database();
-		$result = $db->query('SELECT access_token FROM pages WHERE cp_domain = :domain;',array(':domain' => $domainString));
-	        return $result[0]["access_token"];
-
-	} catch(PDOException $e) {
-        	echo "Could not find proper configuration for " . $domainString . ".confessionspage.com.";
-	}
-}
 ?>
