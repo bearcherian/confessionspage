7b91751d3ebeda8db231b1da7e59cacf0ae00551
bearcherian <cherian.benjamin@gmail.com>
2013-03-18 02:42:21 +0000

    finishing form layout

--- post/index.php
+++ post/index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" src="//normalize-css.googlecode.com/svn/trunk/normalize.css" />
+<link rel="stylesheet" type="text/css" href="css/styles.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,700,400italic' rel='stylesheet' type='text/css'>
 <script>
 function submitConfession() {
 	var post = $("#confession").val();
@@ -12,27 +15,41 @@ function submitConfession() {
 		type: "POST",
 		data: {confession: post},
 		success: function() {
-			$("#complete").html("<h4>Confession Submitted</h4><p>" + post + "</p>");
+			$("#complete").html("<h4>We got it!</h4><p id='confirmation'>" + post + "</p>");
 		},
 		error: function() {
-			$("#complete").html("<h4>Problem with submission</h4>");
+			$("#complete").html("<h4>Sorry, something broke. Try agian in a litte bit.</h4>");
 		}
 	});
 	return false;
 }
 
-//Bind function to form submit
-$(document).ready(function() {
-	//$("#cform").on("submit",submitConfession);
+$(document).ready(function() {  
+    $("textarea[maxlength]").bind("keyup input paste", function() {
+        var limit = parseInt($(this).attr('maxlength'));  
+        var text = $(this).val();  
+        var chars = text.length;  
+  
+        if(chars > limit){  
+            var new_text = text.substr(0, limit);   
+            $(this).val(new_text);  
+        }  
+    });  
 });
+
 </script>
 <title>Confessions form</title>
 </head>
 <body>
 <div id="confessionform">
+	<p>
+	Submit your confession:
+	</p>
 	<form id="cform" method="post">
-	<textarea id="confession" name="confession" rows="5" cols="50"></textarea>
-	<input type="button" value="Submit" onclick="submitConfession();">
+	<textarea maxlength="512" id="confession" name="confession" rows="10" cols="50"></textarea>
+	<button id="submit" type="button" value="Submit" onclick="submitConfession();">
+	Submit
+	</button>
 	</form>
 </div>
 <div id="complete"></div>
