40de44bd2eca20cd5b202f83e2083f9d96b6f088
bearcherian <cherian.benjamin@gmail.com>
2013-03-18 20:33:46 +0000

    Fixing mismatched operator

--- core/process/filter.php
+++ core/process/filter.php
@@ -11,7 +11,7 @@ class Filter {
 	}
 	
         function isApproved() {
-                return ($this->confession->status = "approved");
+                return ((bool)($this->confession->status == "approved"));
         }
 
 	/**
